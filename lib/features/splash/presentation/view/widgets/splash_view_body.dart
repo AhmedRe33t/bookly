548b5478f07a170530b05c367963180e75fb7ba7
ahmedrefaat <arrefaat171@gmail.com>
2024-09-28 05:22:50 +0300

    animation

--- lib/features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,21 +1,59 @@
 import 'package:bookapp/core/utility/app_assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController; //give me vlue 0 to 1
+  late Animation<Offset> sliderTextAnimation; //get value
+    late Animation<Offset> sliderImageAnimation; //get value
+
+  @override
+  void initState() {
+
+    super.initState();
+    animationController=AnimationController(vsync: this,duration:const Duration(seconds: 2));
+    sliderTextAnimation =Tween<Offset>(begin:const Offset(0, 4) ,end:Offset.zero ).animate(animationController);
+     sliderImageAnimation =Tween<Offset>(begin:const Offset(0, -4) ,end:Offset.zero ).animate(animationController);
+   animationController.forward();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return  Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       mainAxisAlignment: MainAxisAlignment.center,
       
 
             children: [
-              Image(image: AssetImage(Assets.imagesLogo)),
-              SizedBox(height: 10,),
-               Text('Read free books',
-               textAlign: TextAlign.center,)
+              AnimatedBuilder(
+                animation:sliderImageAnimation ,
+                builder: (context,_) {
+                  return SlideTransition(
+                    position:sliderImageAnimation ,
+                    child: Image(image: AssetImage(Assets.imagesLogo)));
+                }
+              ),
+              AnimatedBuilder(
+                animation: sliderTextAnimation,
+                builder: (context,_) {
+                  return SlideTransition(
+                    position: sliderTextAnimation,
+                  child: const   Text('Read free books',
+               textAlign: TextAlign.center,) );
+                }, 
+              ),
+            const  SizedBox(height: 10,),
+           
             ],
     );
   }
