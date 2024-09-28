6f6f4cb285a31ecd0a127302bce07734027adc38
ahmedrefaat <arrefaat171@gmail.com>
2024-09-28 04:24:13 +0300

    spalsh view

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,12 @@
+import 'package:bookapp/features/splash/presentation/view/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router=GoRouter(routes: [
+  
+  GoRoute(path: '/',builder: (context, state) => const SplashView(),),
+
+
+
+
+
+]);
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bookapp/core/routes/app_routes.dart';
+import 'package:bookapp/core/utility/app_colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+       theme: ThemeData(
+          scaffoldBackgroundColor: AppColors.KPrimaryColr
+        ),
+        debugShowCheckedModeBanner: false,
+        routerConfig: router,
+    );
+  }
+}
